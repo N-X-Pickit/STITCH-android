4cc10a971fae58325f2db9546734de8486766613
seunggyu <seunggyu97@gmail.com>
2023-02-17 18:56:34 +0900

    #8 : 스플래시 화면 구현(임시)

---
+++ app/src/main/java/com/seunggyu/stitch/ui/SplashActivity.kt
@@ -0,0 +1,50 @@
+package com.seunggyu.stitch.ui
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.lifecycle.lifecycleScope
+import com.seunggyu.stitch.databinding.ActivitySplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity: AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+
+        // background 영역을 statusbar까지 확장
+        this.window?.apply {
+            this.statusBarColor = Color.TRANSPARENT
+            WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars = false
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                setDecorFitsSystemWindows(false)
+            } else {
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
+        }
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(2000)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+    }
+}
