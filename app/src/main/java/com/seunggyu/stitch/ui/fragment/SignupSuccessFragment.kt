55987bc261ef95b29e8ac51e4349d186bfbf5bd1
seunggyu <seunggyu97@gmail.com>
2023-02-22 14:51:23 +0900

    #14 : 회원가입 완료 View

---
+++ app/src/main/java/com/seunggyu/stitch/ui/fragment/SignupSuccessFragment.kt
@@ -0,0 +1,34 @@
+package com.seunggyu.stitch.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.seunggyu.stitch.adapter.SignupProfileListAdapter
+import com.seunggyu.stitch.databinding.FragSignup2Binding
+import com.seunggyu.stitch.databinding.FragSignupSuccessBinding
+import com.seunggyu.stitch.viewModel.SignupViewModel
+
+class SignupSuccessFragment : Fragment() {
+    private val viewModel by viewModels<SignupViewModel>()
+
+    private val binding by lazy {
+        FragSignupSuccessBinding.inflate(layoutInflater)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        with(binding) {
+
+        }
+
+        return binding.root
+    }
+}
