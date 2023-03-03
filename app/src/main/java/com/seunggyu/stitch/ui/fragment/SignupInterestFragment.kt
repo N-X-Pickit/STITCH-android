e9baa64e8d324ea14a01781552e4e25c91b8991c
seunggyu <seunggyu97@gmail.com>
2023-03-03 18:44:37 +0900

    #20 : 관심운동 선택시 블러처리 기능 구현

--- app/src/main/java/com/seunggyu/stitch/ui/fragment/SignupInterestFragment.kt
+++ app/src/main/java/com/seunggyu/stitch/ui/fragment/SignupInterestFragment.kt
@@ -1,10 +1,17 @@
 package com.seunggyu.stitch.ui.fragment
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.renderscript.Allocation
+import android.renderscript.RenderScript
+import android.renderscript.ScriptIntrinsicBlur
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -13,7 +20,10 @@ import androidx.fragment.app.viewModels
 import com.seunggyu.stitch.R
 import com.seunggyu.stitch.databinding.FragInterestBinding
 import com.seunggyu.stitch.viewModel.SignupViewModel
+import android.renderscript.Element
+import jp.wasabeef.blurry.Blurry
 
+@Suppress("DEPRECATION")
 class SignupInterestFragment : Fragment() {
     private val viewModel by activityViewModels<SignupViewModel>()
 
@@ -38,11 +48,17 @@ class SignupInterestFragment : Fragment() {
                 val isSelected: Boolean? = viewModel.checkInteresting("tennis")
                 if (isSelected == false) {
                     // 선택 되었을 경우
-                    cvTennis.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    viewTransparentTennis.isVisible = true
+                    Blurry.with(binding.cvTennis.context).radius(6).sampling(2).onto(binding.cvTennis)
+
                     ivLovelyTennis.isVisible = true
+                    cvTennis.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("tennis")
                 } else {
                     // 선택 해제된 경우
+                    viewTransparentTennis.isVisible = false
+                    Blurry.delete(binding.cvTennis)
+
                     cvTennis.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyTennis.isVisible = false
                     viewModel.deleteInterestingSelectedItem("tennis")
@@ -52,10 +68,18 @@ class SignupInterestFragment : Fragment() {
             cvBadminton.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("badminton")
                 if (isSelected == false) {
-                    cvBadminton.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentBadminton.isVisible = true
+                    Blurry.with(binding.cvBadminton.context).radius(6).sampling(2).onto(binding.cvBadminton)
+
                     ivLovelyBadminton.isVisible = true
+                    cvBadminton.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("badminton")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentBadminton.isVisible = false
+                    Blurry.delete(binding.cvBadminton)
+
                     cvBadminton.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyBadminton.isVisible = false
                     viewModel.deleteInterestingSelectedItem("badminton")
@@ -65,10 +89,18 @@ class SignupInterestFragment : Fragment() {
             cvPingpong.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("pingpong")
                 if (isSelected == false) {
-                    cvPingpong.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentPingpong.isVisible = true
+                    Blurry.with(binding.cvPingpong.context).radius(6).sampling(2).onto(binding.cvPingpong)
+
                     ivLovelyPingpong.isVisible = true
+                    cvPingpong.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("pingpong")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentPingpong.isVisible = false
+                    Blurry.delete(binding.cvPingpong)
+
                     cvPingpong.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyPingpong.isVisible = false
                     viewModel.deleteInterestingSelectedItem("pingpong")
@@ -78,10 +110,18 @@ class SignupInterestFragment : Fragment() {
             cvSoccer.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("soccer")
                 if (isSelected == false) {
-                    cvSoccer.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentSoccer.isVisible = true
+                    Blurry.with(binding.cvSoccer.context).radius(6).sampling(2).onto(binding.cvSoccer)
+
                     ivLovelySoccer.isVisible = true
+                    cvSoccer.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("soccer")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentSoccer.isVisible = false
+                    Blurry.delete(binding.cvSoccer)
+
                     cvSoccer.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelySoccer.isVisible = false
                     viewModel.deleteInterestingSelectedItem("soccer")
@@ -91,10 +131,18 @@ class SignupInterestFragment : Fragment() {
             cvRunning.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("running")
                 if (isSelected == false) {
-                    cvRunning.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentRunning.isVisible = true
+                    Blurry.with(binding.cvRunning.context).radius(6).sampling(2).onto(binding.cvRunning)
+
                     ivLovelyRunning.isVisible = true
+                    cvRunning.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("running")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentRunning.isVisible = false
+                    Blurry.delete(binding.cvRunning)
+
                     cvRunning.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyRunning.isVisible = false
                     viewModel.deleteInterestingSelectedItem("running")
@@ -104,10 +152,18 @@ class SignupInterestFragment : Fragment() {
             cvMountain.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("mountain")
                 if (isSelected == false) {
-                    cvMountain.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentMountain.isVisible = true
+                    Blurry.with(binding.cvMountain.context).radius(6).sampling(2).onto(binding.cvMountain)
+
                     ivLovelyMountain.isVisible = true
+                    cvMountain.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("mountain")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentMountain.isVisible = false
+                    Blurry.delete(binding.cvMountain)
+
                     cvMountain.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyMountain.isVisible = false
                     viewModel.deleteInterestingSelectedItem("mountain")
@@ -117,10 +173,18 @@ class SignupInterestFragment : Fragment() {
             cvHealth.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("health")
                 if (isSelected == false) {
-                    cvHealth.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentHealth.isVisible = true
+                    Blurry.with(binding.cvHealth.context).radius(6).sampling(2).onto(binding.cvHealth)
+
                     ivLovelyHealth.isVisible = true
+                    cvHealth.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("health")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentHealth.isVisible = false
+                    Blurry.delete(binding.cvHealth)
+
                     cvHealth.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyHealth.isVisible = false
                     viewModel.deleteInterestingSelectedItem("health")
@@ -130,10 +194,18 @@ class SignupInterestFragment : Fragment() {
             cvBasketball.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("basketball")
                 if (isSelected == false) {
-                    cvBasketball.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentBasketball.isVisible = true
+                    Blurry.with(binding.cvBasketball.context).radius(6).sampling(2).onto(binding.cvBasketball)
+
                     ivLovelyBasketball.isVisible = true
+                    cvBasketball.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("basketball")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentBasketball.isVisible = false
+                    Blurry.delete(binding.cvBasketball)
+
                     cvBasketball.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyBasketball.isVisible = false
                     viewModel.deleteInterestingSelectedItem("basketball")
@@ -143,10 +215,18 @@ class SignupInterestFragment : Fragment() {
             cvBaseball.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("baseball")
                 if (isSelected == false) {
-                    cvBaseball.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentBaseball.isVisible = true
+                    Blurry.with(binding.cvBaseball.context).radius(6).sampling(2).onto(binding.cvBaseball)
+
                     ivLovelyBaseball.isVisible = true
+                    cvBaseball.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("baseball")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentBaseball.isVisible = false
+                    Blurry.delete(binding.cvBaseball)
+
                     cvBaseball.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyBaseball.isVisible = false
                     viewModel.deleteInterestingSelectedItem("baseball")
@@ -156,10 +236,18 @@ class SignupInterestFragment : Fragment() {
             cvGolf.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("golf")
                 if (isSelected == false) {
-                    cvGolf.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentGolf.isVisible = true
+                    Blurry.with(binding.cvGolf.context).radius(6).sampling(2).onto(binding.cvGolf)
+
                     ivLovelyGolf.isVisible = true
+                    cvGolf.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("golf")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentGolf.isVisible = false
+                    Blurry.delete(binding.cvGolf)
+
                     cvGolf.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyGolf.isVisible = false
                     viewModel.deleteInterestingSelectedItem("golf")
@@ -169,10 +257,18 @@ class SignupInterestFragment : Fragment() {
             cvEtc.setOnClickListener {
                 val isSelected: Boolean? = viewModel.checkInteresting("etc")
                 if (isSelected == false) {
-                    cvEtc.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
+                    // 선택 되었을 경우
+                    viewTransparentEtc.isVisible = true
+                    Blurry.with(binding.cvEtc.context).radius(6).sampling(2).onto(binding.cvEtc)
+
                     ivLovelyEtc.isVisible = true
+                    cvEtc.strokeColor = ContextCompat.getColor(requireContext(), R.color.primary)
                     viewModel.addInterestingSelectedItem("etc")
                 } else {
+                    // 선택 해제된 경우
+                    viewTransparentEtc.isVisible = false
+                    Blurry.delete(binding.cvEtc)
+
                     cvEtc.strokeColor = ContextCompat.getColor(requireContext(), R.color.gray_12)
                     ivLovelyEtc.isVisible = false
                     viewModel.deleteInterestingSelectedItem("etc")
@@ -180,4 +276,5 @@ class SignupInterestFragment : Fragment() {
             }
         }
     }
+
 }
