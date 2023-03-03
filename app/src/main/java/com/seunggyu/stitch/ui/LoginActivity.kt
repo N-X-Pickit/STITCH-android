84b496757b231c018d58e2467cb2dd7430323146
seunggyu <seunggyu97@gmail.com>
2023-03-04 01:53:29 +0900

    #25 : 구글 로그인 로직

--- app/src/main/java/com/seunggyu/stitch/ui/LoginActivity.kt
+++ app/src/main/java/com/seunggyu/stitch/ui/LoginActivity.kt
@@ -9,16 +9,27 @@ import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.util.Log
 import android.view.View
-import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.toSpannable
 import androidx.core.view.WindowInsetsControllerCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
 import com.seunggyu.stitch.databinding.ActivityLoginBinding
+import com.seunggyu.stitch.viewModel.LoginViewModel
+
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var GoogleSignResultLauncher: ActivityResultLauncher<Intent>
+    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,13 +59,13 @@ class LoginActivity : AppCompatActivity() {
 
         binding.tvPrivacyAgree.text.toSpannable().setSpan(object : ClickableSpan() {
             override fun onClick(p0: View) {
-                Log.d("MyTag","clicked!")
+                Log.d("MyTag", "clicked!")
             }
         }, textServiceStartPoint, textServiceEndPoint, Spanned.SPAN_EXCLUSIVE_INCLUSIVE)
 
         binding.tvPrivacyAgree.text.toSpannable().setSpan(object : ClickableSpan() {
             override fun onClick(p0: View) {
-                Log.d("MyTag","clicked!")
+                Log.d("MyTag", "clicked!")
             }
         }, textPrivacyStartPoint, textPrivacyEndPoint, Spanned.SPAN_EXCLUSIVE_INCLUSIVE)
         binding.tvPrivacyAgree.movementMethod = LinkMovementMethod.getInstance()
@@ -62,25 +73,63 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun init() {
+        // 구글 로그인 로직
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        GoogleSignResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){ result ->
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(task)
+        }
 
         with(binding) {
             btnKakaoLogin.setOnClickListener {
                 if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@LoginActivity)) {
-                    UserApiClient.instance.loginWithKakaoTalk(this@LoginActivity, callback = kakaoCallback)
+                    UserApiClient.instance.loginWithKakaoTalk(
+                        this@LoginActivity,
+                        callback = kakaoCallback
+                    )
                 } else {
-                    UserApiClient.instance.loginWithKakaoAccount(this@LoginActivity, callback = kakaoCallback)
+                    UserApiClient.instance.loginWithKakaoAccount(
+                        this@LoginActivity,
+                        callback = kakaoCallback
+                    )
                 }
             }
             btnGoogleLogin.setOnClickListener {
-                /* TODO : 구글 로그인 기능 구현 */
+                var signIntent: Intent = mGoogleSignInClient.getSignInIntent()
+                GoogleSignResultLauncher.launch(signIntent)
             }
         }
     }
-    internal val kakaoCallback : (OAuthToken?, Throwable?) -> Unit = { token, error ->
+
+    fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            viewModel.setGoogleEmail(account?.email.toString())
+            viewModel.setGoogleToken(account?.idToken.toString())
+            viewModel.setGoogleTokenAuth(account?.serverAuthCode.toString())
+            viewModel.setGoogleNickName(account?.displayName.toString())
+            viewModel.setGooglePhotoUrl(account?.photoUrl.toString())
+
+            Log.e("Google account Email",viewModel.getGoogleEmail())
+            Log.e("Google account Token",viewModel.getGoogleToken())
+            Log.e("Google account TokenAuth", viewModel.getGoogleTokenAuth())
+            Log.e("Google account NickName", viewModel.getGoogleNickName())
+            Log.e("Google account PhotoUrl", viewModel.getGooglePhotoUrl())
+        } catch (e: ApiException){
+            Log.e("Google account","signInResult:failed Code = " + e.statusCode)
+        }
+    }
+
+    private val kakaoCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
         if (error != null) {
-            Log.e("kakao result","로그인 실패- $error")
+            Log.e("kakao result", "로그인 실패- $error")
         } else if (token != null) {
-            Log.e("kakao result","로그인성공 - 토큰 ${token.accessToken} id토큰 ${token.idToken}")
+            Log.e("kakao result", "로그인성공 - 토큰 ${token.accessToken} id토큰 ${token.idToken}")
 
             val intent = Intent(this@LoginActivity, SignupActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/seunggyu/stitch/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/seunggyu/stitch/viewModel/LoginViewModel.kt
@@ -11,14 +11,72 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
-    private val _currentPage = MutableLiveData<Int>()
+    private var _googleEmail = MutableLiveData<String>()
+    private var _googleToken = MutableLiveData<String>()
+    private var _googleTokenAuth = MutableLiveData<String>()
+    private var _googleNickName = MutableLiveData<String>()
+    private var _googlePhotoUrl = MutableLiveData<String>()
 
-    val currentPage: LiveData<Int>
-        get() = _currentPage
+    val googleEmail: LiveData<String>
+        get() = _googleEmail
+
+    val googleToken: LiveData<String>
+        get() = _googleToken
+
+    val googleTokenAuth: LiveData<String>
+        get() = _googleTokenAuth
+
+    val googleNickName: LiveData<String>
+        get() = _googleNickName
+
+    val googlePhotoUrl: LiveData<String>
+        get() = _googlePhotoUrl
 
     init {
-        _currentPage.value = 1
+        _googleEmail.value = ""
+        _googleToken.value = ""
+        _googleTokenAuth.value = ""
+        _googleNickName.value = ""
+        _googlePhotoUrl.value = ""
     }
 
+    fun setGoogleEmail(email: String) {
+        _googleEmail.value = email
+    }
 
+    fun getGoogleEmail() : String {
+        return _googleEmail.value.toString()
+    }
+
+    fun setGoogleToken(token: String) {
+        _googleToken.value = token
+    }
+
+    fun getGoogleToken() : String {
+        return _googleToken.value.toString()
+    }
+
+    fun setGoogleTokenAuth(tokenAuth: String) {
+        _googleTokenAuth.value = tokenAuth
+    }
+
+    fun getGoogleTokenAuth() : String {
+        return _googleTokenAuth.value.toString()
+    }
+
+    fun setGoogleNickName(name: String) {
+        _googleNickName.value = name
+    }
+
+    fun getGoogleNickName() : String {
+        return _googleNickName.value.toString()
+    }
+
+    fun setGooglePhotoUrl(photoUrl: String) {
+        _googlePhotoUrl.value = photoUrl
+    }
+
+    fun getGooglePhotoUrl() : String {
+        return _googlePhotoUrl.value.toString()
+    }
 }
